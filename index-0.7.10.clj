e20316189e9370bc914263c45899bdacdeaed276
Tom Faulhaber <git_com@infolace.com>
2016-06-06 12:52:29 -0700

    Autodoc commit for master/d56cf700

--- index-0.7.10.clj
+++ index-0.7.10.clj
@@ -4,8 +4,7 @@
    :author
    "Stuart Sierra, Michel Salim, Luc Préfontaine, Jonathan Fischer Friberg, Michał Marczyk, Don Jackson",
    :name "clojure.tools.trace",
-   :wiki-url
-   "http://clojure.github.com/tools.trace/clojure.tools.trace-api.html",
+   :wiki-url "http://clojure.github.io/tools.trace/index.html",
    :source-url
    "https://github.com/clojure/tools.trace/blob/d56cf7003f9cf024e1fd15ace0944e8b4793953d/src/main/clojure/clojure/tools/trace.clj"}),
  :vars
@@ -22,7 +21,7 @@
    "Use in place of defn; traces each call/return of this fn, including\narguments. Nested calls to deftrace'd functions will print a\ntree-like structure.\nThe first argument of the form definition can be a doc string",
    :namespace "clojure.tools.trace",
    :wiki-url
-   "http://clojure.github.com/tools.trace//clojure.tools.trace-api.html#clojure.tools.trace/deftrace"}
+   "http://clojure.github.io/tools.trace//index.html#clojure.tools.trace/deftrace"}
   {:raw-source-url
    "https://github.com/clojure/tools.trace/raw/d56cf7003f9cf024e1fd15ace0944e8b4793953d/src/main/clojure/clojure/tools/trace.clj",
    :name "dotrace",
@@ -36,7 +35,7 @@
    "Given a sequence of function identifiers, evaluate the body\nexpressions in an environment in which the identifiers are bound to\nthe traced functions. Does not work on inlined functions,\nsuch as clojure.core/+",
    :namespace "clojure.tools.trace",
    :wiki-url
-   "http://clojure.github.com/tools.trace//clojure.tools.trace-api.html#clojure.tools.trace/dotrace"}
+   "http://clojure.github.io/tools.trace//index.html#clojure.tools.trace/dotrace"}
   {:raw-source-url
    "https://github.com/clojure/tools.trace/raw/d56cf7003f9cf024e1fd15ace0944e8b4793953d/src/main/clojure/clojure/tools/trace.clj",
    :name "trace",
@@ -50,7 +49,7 @@
    "Sends name (optional) and value to the tracer function, then\nreturns value. May be wrapped around any expression without\naffecting the result.",
    :namespace "clojure.tools.trace",
    :wiki-url
-   "http://clojure.github.com/tools.trace//clojure.tools.trace-api.html#clojure.tools.trace/trace"}
+   "http://clojure.github.io/tools.trace//index.html#clojure.tools.trace/trace"}
   {:raw-source-url
    "https://github.com/clojure/tools.trace/raw/d56cf7003f9cf024e1fd15ace0944e8b4793953d/src/main/clojure/clojure/tools/trace.clj",
    :name "trace-forms",
@@ -64,7 +63,7 @@
    "Trace all the forms in the given body. Returns any underlying uncaught exceptions that may make the forms fail.",
    :namespace "clojure.tools.trace",
    :wiki-url
-   "http://clojure.github.com/tools.trace//clojure.tools.trace-api.html#clojure.tools.trace/trace-forms"}
+   "http://clojure.github.io/tools.trace//index.html#clojure.tools.trace/trace-forms"}
   {:raw-source-url
    "https://github.com/clojure/tools.trace/raw/d56cf7003f9cf024e1fd15ace0944e8b4793953d/src/main/clojure/clojure/tools/trace.clj",
    :name "trace-ns",
@@ -78,7 +77,7 @@
    "Trace all fns in the given name space. The given name space can be quoted, unquoted or stored in a var.\nWe must try to resolve the expression passed to us partially to find out if it needs to be quoted or not\nwhen passed to trace-ns*",
    :namespace "clojure.tools.trace",
    :wiki-url
-   "http://clojure.github.com/tools.trace//clojure.tools.trace-api.html#clojure.tools.trace/trace-ns"}
+   "http://clojure.github.io/tools.trace//index.html#clojure.tools.trace/trace-ns"}
   {:raw-source-url
    "https://github.com/clojure/tools.trace/raw/d56cf7003f9cf024e1fd15ace0944e8b4793953d/src/main/clojure/clojure/tools/trace.clj",
    :name "trace-vars",
@@ -92,7 +91,7 @@
    "Trace each of the specified Vars.\nThe arguments may be Var objects or symbols to be resolved in the current\nnamespace.",
    :namespace "clojure.tools.trace",
    :wiki-url
-   "http://clojure.github.com/tools.trace//clojure.tools.trace-api.html#clojure.tools.trace/trace-vars"}
+   "http://clojure.github.io/tools.trace//index.html#clojure.tools.trace/trace-vars"}
   {:raw-source-url
    "https://github.com/clojure/tools.trace/raw/d56cf7003f9cf024e1fd15ace0944e8b4793953d/src/main/clojure/clojure/tools/trace.clj",
    :name "traceable?",
@@ -105,7 +104,7 @@
    :doc "Returns true if the given var can be traced, false otherwise",
    :namespace "clojure.tools.trace",
    :wiki-url
-   "http://clojure.github.com/tools.trace//clojure.tools.trace-api.html#clojure.tools.trace/traceable?"}
+   "http://clojure.github.io/tools.trace//index.html#clojure.tools.trace/traceable?"}
   {:raw-source-url
    "https://github.com/clojure/tools.trace/raw/d56cf7003f9cf024e1fd15ace0944e8b4793953d/src/main/clojure/clojure/tools/trace.clj",
    :name "traced?",
@@ -119,7 +118,7 @@
    "Returns true if the given var is currently traced, false otherwise",
    :namespace "clojure.tools.trace",
    :wiki-url
-   "http://clojure.github.com/tools.trace//clojure.tools.trace-api.html#clojure.tools.trace/traced?"}
+   "http://clojure.github.io/tools.trace//index.html#clojure.tools.trace/traced?"}
   {:raw-source-url
    "https://github.com/clojure/tools.trace/raw/d56cf7003f9cf024e1fd15ace0944e8b4793953d/src/main/clojure/clojure/tools/trace.clj",
    :name "untrace-ns",
@@ -133,7 +132,7 @@
    "Untrace all fns in the given name space. The given name space can be quoted, unquoted or stored in a var.\nWe must try to resolve the expression passed to us partially to find out if it needs to be quoted or not\nwhen passed to untrace-ns*",
    :namespace "clojure.tools.trace",
    :wiki-url
-   "http://clojure.github.com/tools.trace//clojure.tools.trace-api.html#clojure.tools.trace/untrace-ns"}
+   "http://clojure.github.io/tools.trace//index.html#clojure.tools.trace/untrace-ns"}
   {:raw-source-url
    "https://github.com/clojure/tools.trace/raw/d56cf7003f9cf024e1fd15ace0944e8b4793953d/src/main/clojure/clojure/tools/trace.clj",
    :name "untrace-vars",
@@ -147,7 +146,7 @@
    "Untrace each of the specified Vars.\nReverses the effect of trace-var / trace-vars / trace-ns for each\nof the arguments, replacing the traced functions with the original,\nuntraced versions.",
    :namespace "clojure.tools.trace",
    :wiki-url
-   "http://clojure.github.com/tools.trace//clojure.tools.trace-api.html#clojure.tools.trace/untrace-vars"}
+   "http://clojure.github.io/tools.trace//index.html#clojure.tools.trace/untrace-vars"}
   {:raw-source-url
    "https://github.com/clojure/tools.trace/raw/d56cf7003f9cf024e1fd15ace0944e8b4793953d/src/main/clojure/clojure/tools/trace.clj",
    :name "ThrowableRecompose",
@@ -161,14 +160,14 @@
    "Protocol to isolate trace-form from convoluted throwables that \ndo not have a constructor with a single string argument.\n\nclone-throwable attempts to clone a throwable with a human readable stack trace\nand message :)\nIt must return a throwable of the same class. If not clonable, the original\nthrowable should be returned. At least this will preserve the original \nthrowable information.\n\nCloning should be non-obtrusive hence internal exceptions should be silently\nswallowed and return the original throwable.",
    :namespace "clojure.tools.trace",
    :wiki-url
-   "http://clojure.github.com/tools.trace//clojure.tools.trace-api.html#clojure.tools.trace/ThrowableRecompose"}
+   "http://clojure.github.io/tools.trace//index.html#clojure.tools.trace/ThrowableRecompose"}
   {:name "clone-throwable",
    :doc nil,
    :var-type "function",
    :namespace "clojure.tools.trace",
    :arglists ([this stack-trace args]),
    :wiki-url
-   "http://clojure.github.com/tools.trace//clojure.tools.trace-api.html#clojure.tools.trace/clone-throwable",
+   "http://clojure.github.io/tools.trace//index.html#clojure.tools.trace/clone-throwable",
    :source-url nil,
    :raw-source-url nil,
    :file nil})}
